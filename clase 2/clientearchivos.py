4ffaa46cb8955e5cd1418e13a6bdd7edb54f45ba
jhoan sebastian <jhoanse@utp.edu.co>
2019-04-11 12:03:16 -0500

    cliente y servidor final

--- clase 2/clientearchivos.py
+++ clase 2/clientearchivos.py
@@ -4,6 +4,13 @@ import os
 import sys
 from math import ceil
 
+import hashlib
+
+def encrypt_string(hash_string):
+    sha_signature = \
+        hashlib.sha256(hash_string.encode()).hexdigest()
+    return sha_signature
+
 #10.253.4.31
 
 if __name__ == "__main__":
@@ -12,8 +19,9 @@ if __name__ == "__main__":
 	archivo = parametros[2]
 	print(archivo)
 	context = zmq.Context()
+	envio=1024*1024*10
 	#  Socket to talk to server
-	print("Connecting to hello world server...")
+	print("Connecting to hello world server...\n\n")
 	socket = context.socket(zmq.REQ) #me indica como van a interactuar cliente y servidor (REQ-> request)
 	socket.connect("tcp://localhost:5555")
 	socket.send_multipart([parametros[1].encode(), archivo.encode()])
@@ -32,21 +40,42 @@ if __name__ == "__main__":
 		message = socket.recv()
 		print(message.decode())
 		with open (archivo, 'rb') as archivoenviado:
-			for i in range(0,ceil(os.path.getsize(archivo)/1024)):
-				archivoenviado.seek(i*1024)
-				t=archivoenviado.read(1024)
+			for i in range(0,ceil(os.path.getsize(archivo)/envio)):
+				archivoenviado.seek(i*envio)
+				t=archivoenviado.read(envio)
 				socket.send(t)
 				message = socket.recv()
+		socket.send(b"todo correcto")
+		message=socket.recv()
+		print(message.decode())
 
 
 	if message == b"bajararchivos":
 		socket.send(b"estan bajando un archivo")
 		tamaño = socket.recv()
 		print(tamaño.decode())
-		for i in range (0,ceil(int(tamaño)/1024)):
+		for i in range (0,ceil(int(tamaño)/envio)):
 			socket.send(b"ready")
 			archivito = socket.recv()
-			print(archivito.decode())
+			#print(archivito.decode())
 			with open(archivo, 'ab') as archivobajado:
 				archivobajado.write(archivito)
+
+
+	if message== b"listar":
+		socket.send(b"enlistando archivos")
+		print ("Los archivos que hay en el servidor son: \n\n")
+		while True:
+			archivo= socket.recv()
+			if(archivo.decode()):
+				print(archivo.decode())
+				socket.send(b"Esperando mas archivos")
+			else:
+				break
+
+	#calcular ram disponible
+	#suponiendo que el tramo es de 1 gb en las mejores condiciones puede tardar 1 sg
+	#segmentar sirve para generar un balanceo de cargas roound 
+	#mi archivo pasa de mi computador a el servidor
+	
 					
--- clase 2/serverarchivos.py
+++ clase 2/serverarchivos.py
@@ -3,28 +3,45 @@ import os
 import time
 import sys
 from math import ceil
+from os import listdir
+import hashlib
+
+def encrypt_string(hash_string):
+    sha_signature = \
+        hashlib.sha256(hash_string.encode()).hexdigest()
+    return sha_signature
+
 
 context = zmq.Context()
 socket = context.socket(zmq.REP) # REP -> Reply
 socket.bind("tcp://*:5555")
+envio=1024*1024*10
+contador=0
 
 
-def subirarchivos(nombrearchivo):
+def subirarchivos(nombrearchivo):   
+    global contador
+    contador=contador+1
+    print(contador)
     filename, file_extension = os.path.splitext(nombrearchivo)
     socket.send(b"subearchivo")
-    tamañoarchivo = socket.recv()
+    tamañoarchivo = socket.recv().decode()
+    filenameencriptado= encrypt_string(filename+tamañoarchivo+str(contador))
     socket.send(b"procediendo a subir archivo")
-    for i in range (0,ceil(int(tamañoarchivo)/1024)):
+    for i in range (0,ceil(int(tamañoarchivo)/envio)):
         archivito = socket.recv()
-        with open("archivosubido"+file_extension, 'ab') as archivo:
+        with open(filenameencriptado+file_extension, 'ab') as archivo:
             archivo.write(archivito)
             socket.send(b"ready")
+    socket.recv()
+    socket.send(b"El nombre para descargar el archivo "+filename.encode()+file_extension.encode()+ b" es "+filenameencriptado.encode()+file_extension.encode())
+
 
 
 def bajararchivos(nombrearchivobajar):
     filename, file_extension = os.path.splitext(nombrearchivobajar)
     size= os.path.getsize(nombrearchivobajar)
-    print(ceil(size/1024))
+    print(ceil(size/envio))
 
     socket.send(b"bajararchivos")
     message=socket.recv()
@@ -32,14 +49,22 @@ def bajararchivos(nombrearchivobajar):
     socket.send(str(size).encode())
 
     with open (nombrearchivobajar, 'rb') as archivoenviado:
-        for i in range(0,ceil(size/1024)): 
+        for i in range(0,ceil(size/envio)): 
             message = socket.recv()
-            archivoenviado.seek(i*1024)
-            t=archivoenviado.read(1024)
+            archivoenviado.seek(i*envio)
+            t=archivoenviado.read(envio)
             print("hola server")
             socket.send(t)
             
 
+def archivosservidor(ruta = '.'):
+    socket.send(b"listar")
+    socket.recv()
+    archivos=listdir(ruta)
+    for archivitos in archivos:
+        socket.send(archivitos.encode())
+        socket.recv()
+    socket.send(b"")
 
 
 if __name__ == "__main__":
@@ -52,7 +77,8 @@ if __name__ == "__main__":
         if funcion == b"bajararchivos":
             bajararchivos(archivo.decode())
 
-
+        if funcion == b"listar":
+            archivosservidor()
 
 """
 
