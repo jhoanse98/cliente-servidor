3bfde1b5866f646ebb5d77ceef4a6d40000a1ec1
jhoanse98 <32344473+jhoanse98@users.noreply.github.com>
2019-05-20 13:50:04 -0500

    modificaciones pequeñas servidor

--- tarea3/tarea3/server.py
+++ tarea3/tarea3/server.py
@@ -1,18 +1,22 @@
 import zmq
 import sys
 
+MAX_BUFFER=1024*1024*100
+
 if __name__ == "__main__":
     context = zmq.Context()
+    IpProxy = sys.argv[1]
     socketProxy = context.socket(zmq.REQ)
-    socketProxy.connect("tcp://localhost:5555")
+    socketProxy.connect("tcp://{}:5555".format(IpProxy,PortProxy))
 
+    serveraddress = sys.argv[2] 
+    portserver = sys.argv[3]
 
-    port = sys.argv[1]
-    socketRecv = context.socket(zmq.ROUTER)
-    socketRecv.bind('tcp://*:{}'.format(port))
+    socketRecv = context.socket(zmq.REP)
+    socketRecv.bind('tcp://*:{}'.format(portserver))
 
-    address = 'localhost:{}'.format(port).encode('ascii')
-    numPetitions = sys.argv[2].encode('ascii')
+    address = '{}:{}'.format(serveraddress, portserver).encode('ascii')
+    numPetitions = sys.argv[4].encode('ascii')
     socketProxy.send_multipart([b'server',address,numPetitions])
     response = socketProxy.recv_multipart()
 
@@ -21,17 +25,18 @@ if __name__ == "__main__":
     while True:
         responseClient = socketRecv.recv_multipart()
         print(responseClient[0])
-        if responseClient[1] == b'sending':
-            namefile= responseClient[2].decode()
+        if responseClient[0] == b'sending':
+            namefile= responseClient[1].decode()
             print("Vamos a subir un archivo")
             with open(namefile,'ab') as f:
                         #print(message[0])
-                        f.write(responseClient[3])
-        if responseClient[1] == b'downloading':
+                        f.write(responseClient[2])
+            socketRecv.send_multipart([b'ok'])
+        if responseClient[0] == b'downloading':
             print('bajando un archivo')
-            with open(responseClient[2].decode(),'rb') as download:
-                data=download.read()
-                socketRecv.send_multipart([responseClient[0],b'downloading', data])
+            with open(responseClient[1].decode(),'rb') as download:
+                data=download.read(MAX_BUFFER)
+                socketRecv.send_multipart([b'downloading', data])
                 print('se esta bajando un archivito')
 
 
